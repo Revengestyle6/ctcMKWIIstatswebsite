a4342ec4e67ee03a9d8a4f06cece42bd27490027
faken <ctgpghosts@gmail.com>
2026-01-21 19:03:50 -0500

    new jan21 fixes

--- backend/main.py
+++ backend/main.py
@@ -7,5 +7,4 @@ BASE_DIR = Path(__file__).resolve().parent
 csv_directory = BASE_DIR / "CSV"
 json_directory = BASE_DIR / "JSON"
 
-extract.extract(str(json_directory / "ctc_s1_d3"), str(csv_directory / "ctc_d3.csv"))
-extract.extract(str(json_directory / "ctc_s1_d4"), str(csv_directory / "ctc_d4.csv"))
+stats.findplayeravg("zilla")
